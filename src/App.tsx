3f4cd1a8a255d1f1f4411d290aa005f69b98939c
Muhammad Saqib <muhammadsackib@gmail.com>
2024-08-22 12:41:21 +0500

    put todos in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { useReducer } from "react";
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      value: event.target.elements["new-todo"].value,
+    });
+  };
+
   return (
-    <h1>Hello world</h1>
-  )
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="new-todo"
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+          />
+          <input type="submit" hidden />
+        </form>
+      </header>
+    </section>
+  );
 }
 
-export default App
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TODO": {
+      return [{ id: window.crypto.randomUUID(), todo: action.value }, ...state];
+    }
+  }
+}
+
+export default App;
